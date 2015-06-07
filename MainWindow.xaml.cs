f4145968462d77ed3e6b798c9ed6840ccad131a6
Optimus Prime <yuralightman@gmail.com>
2015-06-08 00:41:14 +0300

    complete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,17 @@ using System.Windows.Shapes;
 
 namespace WpfApplication_Color_Filling
 {
+    public struct Queue
+    {
+        public int x, y;
+
+        public Queue(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -26,11 +37,9 @@ namespace WpfApplication_Color_Filling
         byte blue = 51, green = 51, red = 51;
         byte[] byteArray;
         byte[] startColor;
-
-        int imgHeight, imgWidth;
-        bool down = false;
-
+        
         private Dictionary<int, Action<int, int, int, int>> _algorithms = new Dictionary<int, Action<int, int, int, int>>();
+        List<Queue> queue = new List<Queue>();
 
         private void Swap<T>(ref T lhs, ref T rhs) { T temp; temp = lhs; lhs = rhs; rhs = temp; }
 
@@ -42,13 +51,6 @@ namespace WpfApplication_Color_Filling
 
         private void initImage()
         {
-            //imgHeight = (int)image.Height;
-            //imgWidth = (int)image.Width;
-            //image = new Image();
-            //bm = new WriteableBitmap(imgWidth-1, imgHeight-1, 96, 96, PixelFormats.Bgra32, null);
-            
-            //image.Source = new BitmapImage(new Uri("fro.jpg", UriKind.Relative));
-            //System.Drawing.Pen skyBluePen = new System.Drawing.Pen(System.Drawing.Brushes.Black);
             loadBitmap("fro.png");
             image.Source = bm;
         }
@@ -58,10 +60,8 @@ namespace WpfApplication_Color_Filling
             BitmapSource bmp = BitmapFrame.Create(new Uri(name, UriKind.Relative), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
             if (bmp.Format != PixelFormats.Bgra32)
                 bmp = new FormatConvertedBitmap(bmp, PixelFormats.Bgra32, null, 1);      // Just ignore the last parameter
-            //WriteableBitmap wbmp = new WriteableBitmap(bmp.PixelWidth, bmp.PixelHeight, bmp.DpiX, bmp.DpiY, bmp.Format, bmp.Palette);
             bm = null;
             bm = new WriteableBitmap(700, 480, 96, 96, bmp.Format, bmp.Palette);
-            //bm = new WriteableBitmap(imgWidth - 1, imgHeight - 1, 96, 96, bmp.Format, bmp.Palette);
             Int32Rect r = new Int32Rect(0, 0, 700, 480);
             bm.Lock();
             bmp.CopyPixels(r, bm.BackBuffer, bm.BackBufferStride * 480, bm.BackBufferStride);
@@ -141,8 +141,23 @@ namespace WpfApplication_Color_Filling
             makeByteArray();
             copyPixels();
             setStartColor((int)p.X, (int)p.Y);
-            
-            floodFillRecursion((int)p.X, (int)p.Y);
+
+            int offset = (int)p.Y * bm.PixelWidth * bm.Format.BitsPerPixel / 8 + (int)p.X * bm.Format.BitsPerPixel / 8;
+            if (byteArray[offset] == blue && byteArray[offset + 1] == green
+                && byteArray[offset + 2] == red && byteArray[offset + 3] == 255)
+            {
+                return;
+            }
+
+            switch (comboAlgorithm.SelectedIndex)
+            {
+                case 0: floodFillRecursion((int)p.X, (int)p.Y, 0);
+                    break;
+                default: floodFillLoop((int)p.X, (int)p.Y);
+                    break;
+            }
+            //floodFillRecursion((int)p.X, (int)p.Y, 0);
+
             bm.WritePixels(new Int32Rect(0, 0, bm.PixelWidth, bm.PixelHeight),
                 byteArray,
                 bm.PixelWidth * bm.Format.BitsPerPixel / 8,
@@ -261,18 +276,18 @@ namespace WpfApplication_Color_Filling
             };
         }
 
-        private void floodFillRecursion(int x, int y)
+        private int floodFillRecursion(int x, int y, int n)
         {
-            if (x < 0 || x > bm.PixelWidth - 1 || y < 0 || y > bm.PixelHeight - 1)
+            if (x < 0 || x > bm.PixelWidth - 1 || y < 0 || y > bm.PixelHeight - 1 || n > 8000)
             {
-                return;
+                return n;
             }
-
             int offset = y * bm.PixelWidth * bm.Format.BitsPerPixel / 8 + x * bm.Format.BitsPerPixel / 8;
+
             if (byteArray[offset] != startColor[0] || byteArray[offset + 1] != startColor[1]
                 || byteArray[offset + 2] != startColor[2] || byteArray[offset + 3] != startColor[3])
             {
-                return;
+                return n;
             }
 
             byteArray[offset] = blue;
@@ -280,10 +295,121 @@ namespace WpfApplication_Color_Filling
             byteArray[offset + 2] = red;
             byteArray[offset + 3] = 255;
 
-            floodFillRecursion(x, y - 1);
-            floodFillRecursion(x + 1, y);
-            floodFillRecursion(x, y + 1);
-            floodFillRecursion(x - 1, y);
+            n = floodFillRecursion(x, y - 1, ++n);
+
+            n = floodFillRecursion(x + 1, y, ++n);
+
+            n = floodFillRecursion(x, y + 1, ++n);
+
+            n = floodFillRecursion(x - 1, y, ++n);
+
+            return n;
+        }
+
+        private void floodFillLoop(int x, int y)
+        {
+            //Queue s, f;
+
+            /* Clear buffer */
+            queue.Clear();
+
+            /* Set start offset and check if the color is not equal replacement one */
+            int offset = y * bm.PixelWidth * bm.Format.BitsPerPixel / 8 + x * bm.Format.BitsPerPixel / 8;
+            //if (byteArray[offset] == blue && byteArray[offset + 1] == green
+            //    && byteArray[offset + 2] == red && byteArray[offset + 3] == 255)
+            //    return;
+
+            /* Add start point into buffer */
+            queue.Add(new Queue(x, y));
+
+            /* Check all buffer elements */
+            while (queue.Count > 0)
+            {
+                //s = f = queue[0];
+                int tempX = queue[0].x,
+                    tempY = queue[0].y,
+                    tempOffset = tempY * bm.PixelWidth * bm.Format.BitsPerPixel / 8 
+                    + tempX * bm.Format.BitsPerPixel / 8;
+
+                /* Check all elements to the west which are target-colored */
+                while (tempX > -1 && isSameColorAsChange(tempOffset))
+                {
+                    /* Add north point to List if have to */
+                    if (tempY - 1 > -1 &&
+                        isSameColorAsChange(tempOffset - bm.PixelWidth * bm.Format.BitsPerPixel / 8))
+                    {
+                        queue.Add(new Queue(tempX, tempY - 1));
+                    }
+
+                    /* Add south point to List if have to */
+                    if (tempY + 1 < bm.PixelHeight &&
+                        isSameColorAsChange(tempOffset + bm.PixelWidth * bm.Format.BitsPerPixel / 8))
+                    {
+                        queue.Add(new Queue(tempX, tempY + 1));
+                    }
+
+                    /* Fill temp point */
+                    byteArray[tempOffset] = blue;
+                    byteArray[tempOffset + 1] = green;
+                    byteArray[tempOffset + 2] = red;
+                    byteArray[tempOffset + 3] = 255;
+
+                    /* Go one point left */
+                    tempX--;
+                    tempOffset -= 4;
+                }
+
+                tempX = queue[0].x + 1;
+                tempOffset = tempY * bm.PixelWidth * bm.Format.BitsPerPixel / 8
+                    + tempX * bm.Format.BitsPerPixel / 8; 
+
+                /* Check all elements to Japan which are target-colored  */
+                while (tempX < bm.PixelWidth && isSameColorAsChange(tempOffset))
+                {
+                    /* Add north point to List if have to */
+                    if (tempY - 1 > -1 &&
+                        isSameColorAsChange(tempOffset - bm.PixelWidth * bm.Format.BitsPerPixel / 8))
+                    {
+                        queue.Add(new Queue(tempX, tempY - 1));
+                    }
+
+                    /* Add south point to List if have to */
+                    if (tempY + 1 < bm.PixelHeight &&
+                        isSameColorAsChange(tempOffset + bm.PixelWidth * bm.Format.BitsPerPixel / 8))
+                    {
+                        queue.Add(new Queue(tempX, tempY + 1));
+                    }
+
+                    /* Fill temp point */
+                    byteArray[tempOffset] = blue;
+                    byteArray[tempOffset + 1] = green;
+                    byteArray[tempOffset + 2] = red;
+                    byteArray[tempOffset + 3] = 255;
+
+                    /* Go one point left */
+                    tempX++;
+                    tempOffset += 4;
+                }
+
+                /* Pop curent Q point */
+                queue.RemoveAt(0);
+            }
+        }
+
+        private bool isSameColorAsChange(int offset)
+        {
+            if (offset < 0 || offset > byteArray.Length - 1)
+            {
+                return false;
+            }
+            if (byteArray[offset] == startColor[0] &&
+                    byteArray[offset + 1] == startColor[1] &&
+                    byteArray[offset + 2] == startColor[2] &&
+                    byteArray[offset + 3] == startColor[3])
+            {
+                return true;
+            }
+            return false;
         }
 
         private void buttonFill_Click(object sender, RoutedEventArgs e)

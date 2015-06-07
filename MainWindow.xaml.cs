0f1b28f190ae50ae27b0bf75bc6643357daeb653
Optimus Prime <yuralightman@gmail.com>
2015-06-07 18:35:07 +0300

    half

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Drawing;
 
 namespace WpfApplication_Color_Filling
 {
@@ -20,9 +21,276 @@ namespace WpfApplication_Color_Filling
     /// </summary>
     public partial class MainWindow : Window
     {
+        public WriteableBitmap bm;
+        //public Graphics graphics;
+        byte blue = 51, green = 51, red = 51;
+        byte[] byteArray;
+        byte[] startColor;
+
+        int imgHeight, imgWidth;
+        bool down = false;
+
+        private Dictionary<int, Action<int, int, int, int>> _algorithms = new Dictionary<int, Action<int, int, int, int>>();
+
+        private void Swap<T>(ref T lhs, ref T rhs) { T temp; temp = lhs; lhs = rhs; rhs = temp; }
+
         public MainWindow()
         {
             InitializeComponent();
+            initImage();
         }
+
+        private void initImage()
+        {
+            //imgHeight = (int)image.Height;
+            //imgWidth = (int)image.Width;
+            //image = new Image();
+            //bm = new WriteableBitmap(imgWidth-1, imgHeight-1, 96, 96, PixelFormats.Bgra32, null);
+            
+            //image.Source = new BitmapImage(new Uri("fro.jpg", UriKind.Relative));
+            //System.Drawing.Pen skyBluePen = new System.Drawing.Pen(System.Drawing.Brushes.Black);
+            loadBitmap("fro.png");
+            image.Source = bm;
+        }
+
+        private void loadBitmap(string name)
+        {
+            BitmapSource bmp = BitmapFrame.Create(new Uri(name, UriKind.Relative), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            if (bmp.Format != PixelFormats.Bgra32)
+                bmp = new FormatConvertedBitmap(bmp, PixelFormats.Bgra32, null, 1);      // Just ignore the last parameter
+            //WriteableBitmap wbmp = new WriteableBitmap(bmp.PixelWidth, bmp.PixelHeight, bmp.DpiX, bmp.DpiY, bmp.Format, bmp.Palette);
+            bm = null;
+            bm = new WriteableBitmap(700, 480, 96, 96, bmp.Format, bmp.Palette);
+            //bm = new WriteableBitmap(imgWidth - 1, imgHeight - 1, 96, 96, bmp.Format, bmp.Palette);
+            Int32Rect r = new Int32Rect(0, 0, 700, 480);
+            bm.Lock();
+            bmp.CopyPixels(r, bm.BackBuffer, bm.BackBufferStride * 480, bm.BackBufferStride);
+            bm.AddDirtyRect(r);
+            bm.Unlock();
+            
+            //return wbmp;
+        }
+
+        private void circle(int x1, int y1, int radius1)
+        {
+            int x = 0,
+                y = radius1,
+                delta = 1 - 2 * radius1,
+                error = 0;
+            while (y >= 0)
+            {
+
+                drawPixel(x1 + x, y1 + y);
+                drawPixel(x1 + x, y1 - y);
+                drawPixel(x1 - x, y1 + y);
+                drawPixel(x1 - x, y1 - y);
+                error = 2 * (delta + y) - 1;
+                if ((delta < 0) && (error <= 0))
+                {
+                    delta += 2 * ++x + 1;
+                    continue;
+                }
+                error = 2 * (delta - x) - 1;
+                if ((delta > 0) && (error > 0))
+                {
+                    delta += 1 - 2 * --y;
+                    continue;
+                }
+                x++;
+                delta += 2 * (x - y);
+                y--;
+            }
+        }
+
+        private void drawPixel(int x1, int y1)
+        {
+            if (x1 < 1 || x1 > 699 || y1 < 1 || y1 > 479)
+            {
+                return;
+            }
+            byte[] colorData = { blue, green, red, 255 };
+            Int32Rect rect = new Int32Rect(x1, y1, 1, 1);
+            bm.WritePixels(rect, colorData, 4, 0);
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            setPicture();
+        }
+
+        private void drawCircle(int x, int y)
+        {
+            for (int i = 1; i < 15; i++)
+            {
+                circle(x, y, i);
+            }
+        }
+
+        private void image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Point p = Mouse.GetPosition(image);
+            //drawCircle((int)p.X, (int)p.Y);
+            //down = true;
+            Point p = Mouse.GetPosition(image);
+
+            makeByteArray();
+            copyPixels();
+            setStartColor((int)p.X, (int)p.Y);
+            
+            floodFillRecursion((int)p.X, (int)p.Y);
+            bm.WritePixels(new Int32Rect(0, 0, bm.PixelWidth, bm.PixelHeight),
+                byteArray,
+                bm.PixelWidth * bm.Format.BitsPerPixel / 8,
+                0);
+        }
+
+        private void image_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Point p = Mouse.GetPosition(image);
+
+            //makeByteArray();
+            //copyPixels();
+
+            //floodFillRecursion((int)p.X, (int)p.Y);
+            //if (down)
+            //{
+            //    drawCircle((int)p.X, (int)p.Y);
+            //}
+        }
+                
+        private void comboPicture_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            setPicture();
+        }
+
+        private void setPicture()
+        {
+            switch (comboPicture.SelectedIndex)
+            {
+                case 0: 
+                    loadBitmap("fro.png");
+                    image.Source = bm;
+                    break;
+                case 1: 
+                    loadBitmap("boo.png");
+                    image.Source = bm;
+                    break;
+                default: 
+                    loadBitmap("elf.png");
+                    image.Source = bm;
+                    break;
+            }
+        }
+
+        private void comboColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            setColor(); 
+        }
+
+        private void setColor()
+        {
+            switch (comboColor.SelectedIndex)
+            {
+                case 0:
+                    blue = 255;
+                    green = 255;
+                    red = 255;
+                    break;
+                case 1:
+                    blue = 80;
+                    green = 80;
+                    red = 255;
+                    break;
+                case 2:
+                    blue = 100;
+                    green = 255;
+                    red = 255;
+                    break;
+                case 3:
+                    blue = 31;
+                    green = 220;
+                    red = 31;
+                    break;
+                case 4:
+                    blue = 255;
+                    green = 51;
+                    red = 50;
+                    break;
+                case 5:
+                    blue = 0;
+                    green = 0;
+                    red = 0;
+                    break;
+                case 6:
+                    blue = 153;
+                    green = 0;
+                    red = 255;
+                    break;
+                default:
+                    blue = 0;
+                    green = 51;
+                    red = 51;
+                    break;
+            }
+        }
+
+        private void makeByteArray()
+        {
+            byteArray = new byte[bm.PixelWidth * bm.PixelHeight * bm.Format.BitsPerPixel / 8];
+        }
+
+        private void copyPixels()
+        {
+            bm.CopyPixels(byteArray, 2800, 0);
+        }
+
+        private void setStartColor(int x, int y)
+        {
+            int offset = y * bm.PixelWidth * bm.Format.BitsPerPixel / 8 + x * bm.Format.BitsPerPixel / 8;
+            startColor = new byte[4] 
+            { 
+                byteArray[offset],
+                byteArray[offset + 1], 
+                byteArray[offset + 2], 
+                byteArray[offset + 3] 
+            };
+        }
+
+        private void floodFillRecursion(int x, int y)
+        {
+            if (x < 0 || x > bm.PixelWidth - 1 || y < 0 || y > bm.PixelHeight - 1)
+            {
+                return;
+            }
+
+            int offset = y * bm.PixelWidth * bm.Format.BitsPerPixel / 8 + x * bm.Format.BitsPerPixel / 8;
+            if (byteArray[offset] != startColor[0] || byteArray[offset + 1] != startColor[1]
+                || byteArray[offset + 2] != startColor[2] || byteArray[offset + 3] != startColor[3])
+            {
+                return;
+            }
+
+            byteArray[offset] = blue;
+            byteArray[offset + 1] = green;
+            byteArray[offset + 2] = red;
+            byteArray[offset + 3] = 255;
+
+            floodFillRecursion(x, y - 1);
+            floodFillRecursion(x + 1, y);
+            floodFillRecursion(x, y + 1);
+            floodFillRecursion(x - 1, y);
+        }
+
+        private void buttonFill_Click(object sender, RoutedEventArgs e)
+        {
+            makeByteArray();
+            copyPixels();
+        }
+
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfApplication_Color_Filling.Properties
-{
-
-
+namespace WpfApplication_Color_Filling.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,81 @@ namespace WpfApplication_Color_Filling.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApplication_Color_Filling.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _9 {
+            get {
+                object obj = ResourceManager.GetObject("_9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap boo {
+            get {
+                object obj = ResourceManager.GetObject("boo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap elf {
+            get {
+                object obj = ResourceManager.GetObject("elf", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fro {
+            get {
+                object obj = ResourceManager.GetObject("fro", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
